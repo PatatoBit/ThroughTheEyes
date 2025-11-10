f5706837b7f2e50974ddb6f3f88578c13769eb43
PatatoBit <pathonscript@gmail.com>
2025-11-10 14:59:17 +0700

    Initial Commit

---
+++ through the eyes/through_the_eyesApp.swift
@@ -0,0 +1,17 @@
+//
+//  through_the_eyesApp.swift
+//  through the eyes
+//
+//  Created by Patato on 10/11/25.
+//
+
+import SwiftUI
+
+@main
+struct through_the_eyesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
