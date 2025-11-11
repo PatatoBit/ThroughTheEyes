49bea9c1de739cb130989d30d115daad40765c30
PatatoBit <pathonscript@gmail.com>
2025-11-11 19:03:12 +0700

    layout content and rewind button

--- through the eyes/Tabs/HomeLayout.swift
+++ through the eyes/Tabs/HomeLayout.swift
@@ -8,40 +8,55 @@
 import SwiftUI
 
 struct HomeLayout: View {
+    @AppStorage("hasCompletedOnboarding") private var hasCompletedOnboarding: Bool = false
+    
     let columns = [
-        GridItem(.flexible()),
-        GridItem(.flexible())
+        GridItem(.flexible(), spacing: 20),
+        GridItem(.flexible(), spacing: 20)
     ]
     
     var body: some View {
-        ScrollView {
-            LazyVGrid(columns: columns, spacing: 20) {
-                SimulatorBox(name: "hi")
-                SimulatorBox(name: "hi")
-                SimulatorBox()
+        NavigationStack {
+            ScrollView {
+                LazyVGrid(columns: columns, spacing: 15) {
+                    SimulatorBox(name: "Dyslexia")
+                    SimulatorBox(name: "Color blindness")
+                    SimulatorBox(name: "ADHD")
+                }
+                //            .border(.blue, width: 2)
             }
-            .border(.blue, width: 2)
+            .padding()
+            .toolbar {
+                ToolbarItem (placement: .navigation) {
+                    Button {
+                        hasCompletedOnboarding = false
+                    } label: {
+                        Image(systemName: "memories")
+                            .foregroundColor(.gray)
+                    }
+                }
+            }
         }
     }
 }
 
 struct SimulatorBox: View {
-    var name: String = "No Name"
+    var name: String = "Placeholder"
+    var imageName: String = "placeholder"
     
     var body: some View {
         VStack {
-            Image("yourImageName")
-                    .resizable()
-                    .scaledToFit()
-                    .frame(maxWidth: .infinity)
-                    .aspectRatio(1, contentMode: .fit)
-                    .border(Color.purple, width: 3)
-            
-            
+            Image(imageName)
+                .resizable()
+                .frame(maxWidth: .infinity)
+                .aspectRatio(1, contentMode: .fit)
+                .clipShape(RoundedRectangle(cornerRadius: 10))
+            //                    .border(Color.purple, width: 3)
             Text("\(name)")
         }
         .frame(maxWidth: .infinity)
-        .border(.red, width: 2)
+        
+        //        .border(.red, width: 2)
     }
 }
 
